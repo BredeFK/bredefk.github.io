9a0eaf644259c137b78f0205dc295b81cb47de72
Brede Fritjof Klausen <brede@fritjof.no>
2023-02-27 17:37:17 +0100

    Add ping pong link and quote of the day

---
+++ js/quote.js
@@ -0,0 +1,43 @@
+async function getQuote() {
+    let data = await getQuoteFromLocalStorage()
+    const quoteElement = document.getElementById("quote")
+    quoteElement.innerHTML = '<i>"' + data.quote + '"</i>'
+    quoteElement.title = "Quote of the day by " + data.author
+}
+
+async function getQuoteOfTheDayFromQuotesRest() {
+    const response = await fetch("https://quotes.rest/qod?language=en", {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+
+    if (response.status) {
+        let quoteBody = await response.json()
+        if (quoteBody.contents.quotes[0]) {
+            return quoteBody.contents.quotes[0]
+        }
+    } else {
+        console.error("Could not get Quote of the day")
+    }
+    return null
+}
+
+async function getQuoteFromLocalStorage() {
+    let data = localStorage.getItem(getDate() + "-quote")
+    if (data === null || data === undefined || data === "") {
+        let quote = await getQuoteOfTheDayFromQuotesRest()
+        if (quote !== null) {
+            localStorage.clear()
+            localStorage.setItem(getDate() + "-quote", JSON.stringify(quote))
+            return quote
+        }
+    } else {
+        return JSON.parse(data)
+    }
+}
+
+function getDate() {
+    return new Date().toISOString().split("T")[0]
+}
