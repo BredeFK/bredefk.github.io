1c54dc0670b343d4eed26c5dd4f6ba0a5f7aef91
Brede Fritjof Klausen <brede@fritjof.no>
2023-10-11 22:22:43 +0200

    Add start on Skyrim and display all songs

--- js/song.js
+++ js/song.js
@@ -1,29 +1,83 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const fileList = document.getElementById('file-list');
+    const tocContainer = document.getElementById('table-of-contents');
 
-    // Function to fetch and list text files in the directory
-    function listTextFiles() {
-        fetch('../tabs/')
+
+    // List of known text files
+    const songFiles = [
+        'flaaklypa.txt',
+        'skyrim.txt'
+    ];
+
+    // Function to load and display the content of a text file
+    function displayTextFileContent(songName) {
+        fetch(`../tabs/${songName}`)
             .then(response => response.text())
             .then(data => {
-                const files = data
-                    .split('\n')
-                    .filter(line => line.trim().endsWith('.txt'))
-                    .map(line => line.trim());
+                const songContent = `<pre>${data}</pre>`;
+                const card = document.createElement('div');
+                card.className = 'card text-dark border-dark col-lg-8 col-sm-12 mx-auto mb-5';
 
-                console.log(files)
+                const cardBody = document.createElement('div');
+                cardBody.className = 'card-body';
 
-                if (files.length === 0) {
-                    fileList.innerHTML = 'No song files found';
-                } else {
-                    fileList.innerHTML = files.map(file => `<li><a href="${file}">${file}</a></li>`).join('');
+                const cardTitle = document.createElement('h4');
+                cardTitle.className = 'card-title pb-2';
+                cardTitle.innerHTML = convertDisplayName(songName);
+
+                const songDiv = document.createElement('div');
+                songDiv.id = songName.split('.')[0];
+                songDiv.className = 'song';
+                songDiv.innerHTML = songContent;
+
+                cardBody.appendChild(cardTitle);
+                cardBody.appendChild(songDiv);
+                card.appendChild(cardBody);
+
+                // Dynamically adjust card size based on content length
+                const contentHeight = songDiv.clientHeight;
+                const minHeight = 100;
+                const maxHeight = 600;
+
+                if (contentHeight > minHeight && contentHeight < maxHeight) {
+                    card.style.height = `${contentHeight}px`;
+                } else if (contentHeight >= maxHeight) {
+                    card.style.height = `${maxHeight}px`;
                 }
+
+                fileList.appendChild(card);
+
             })
             .catch(error => {
-                fileList.innerHTML = '<li>Error listing song files.</li>';
+                fileList.insertAdjacentHTML('beforeend', `<li>Error loading ${songName}.</li>`);
                 console.error(error);
             });
     }
 
-    listTextFiles();
+    function convertDisplayName(filename) {
+        let displayName = filename.split('.')[0]
+
+        displayName = displayName.replaceAll("ae", "&#229;")
+        displayName = displayName.replaceAll("oe", "&#248;")
+        displayName = displayName.replaceAll("aa", "&#229;")
+        displayName = displayName.replaceAll("_", " ")
+
+        return displayName[0].toUpperCase() + displayName.substring(1, displayName.length)
+    }
+
+    function generateTableOfContents() {
+        const tocList = document.createElement('ul');
+        songFiles.forEach(fileName => {
+            const displayName = convertDisplayName(fileName);
+            tocList.innerHTML += `<li><a href="#${fileName.split('.')[0]}">${displayName}</a></li>`;
+        });
+
+        tocContainer.appendChild(tocList);
+    }
+
+    generateTableOfContents()
+
+    songFiles.forEach(fileName => {
+        displayTextFileContent(fileName);
+    });
 });
