ceaedc6c774f86c39e94d5aa94aa0cead5abb8b9
Brede Fritjof Klausen <brede@fritjof.no>
2023-12-03 23:00:01 +0100

    Add stopwatch

---
+++ js/stopwatch.js
@@ -0,0 +1,52 @@
+let year = 1997
+let month = 2 - 1
+let day = 13
+let hours = 9
+let minutes = 36
+setInterval(
+    function startStopWatch() {
+
+
+        let birthday = new Date(year, month, day, hours, minutes)
+        let now = new Date()
+        let diff = now - birthday
+
+        const t = {
+            years: Math.floor(Math.floor(diff / (1000 * 60 * 60 * 24)) / 365).toString().padStart(2, '0'),
+            days: (Math.floor(diff / (1000 * 60 * 60 * 24)) % 365).toString().padStart(3, '0'),
+            hours: Math.floor((diff / (1000 * 60 * 60)) % 24).toString().padStart(2, '0'),
+            minutes: Math.floor((diff / (1000 * 60)) % 60).toString().padStart(2, '0'),
+            seconds: Math.floor((diff / 1000) % 60).toString().padStart(2, '0'),
+        }
+        const watch = document.getElementById("watch")
+
+        watch.innerHTML = `<span class="greenColor">${t.years}</span> years : ` +
+            `<span class="greenColor">${t.days}</span> days : ` +
+            `<span class="greenColor">${t.hours}</span> hours : ` +
+            `<span class="greenColor">${t.minutes}</span> minutes : ` +
+            `<span class="greenColor">${t.seconds}</span> seconds`
+
+
+        document.title = `${formatDate(birthday)} - ${formatDate(now)}`
+    }
+    , 100) // Update every 100 milliseconds
+
+function rotationsAroundTheWorld() {
+    const daysToOrbitTheSun = 365.25625
+
+    let diff = Date.now() - new Date(year, month, day, hours, minutes)
+    let years = Math.floor(Math.floor(diff / (1000 * 60 * 60 * 24)) / 365)
+    let days = Math.floor(diff / (1000 * 60 * 60 * 24)) % 365
+
+    let yearsAndDay = years + (days / daysToOrbitTheSun)
+    let numberOfOrbits = (daysToOrbitTheSun * yearsAndDay) / daysToOrbitTheSun
+    const rotations = document.getElementById("rotations")
+    rotations.innerHTML = `<span class="greenColor">${numberOfOrbits}</span> orbits around the sun`
+    rotations.title = `(Days to orbit the sun * years and days you've lived) / Days to orbit the sun` +
+        `\n(${daysToOrbitTheSun} * ${yearsAndDay}) / ${daysToOrbitTheSun}`
+
+}
+
+function formatDate(date) {
+    return `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`
+}
