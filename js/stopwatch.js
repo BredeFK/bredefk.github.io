2888957b05b366714d8f274dba313dae9909805b
Brede Fritjof Klausen <brede@fritjof.no>
2023-12-04 17:55:30 +0100

    Add jul

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,40 +1,43 @@
-let year = 1997
-let month = 2 - 1
-let day = 13
-let hours = 9
-let minutes = 36
+let year = new Date().getFullYear()
+let month = 12 - 1
+let day = 24
+
+const queryString = new URLSearchParams(window.location.search)
 setInterval(
     function startStopWatch() {
 
+        let date = new Date(year, month, day)
 
-        let birthday = new Date(year, month, day, hours, minutes)
+        if (queryString.has("date")) {
+            date = new Date(queryString.get("date"))
+        }
+
         let now = new Date()
-        let diff = now - birthday
+        let diff = date - now
 
         const t = {
-            years: Math.floor(Math.floor(diff / (1000 * 60 * 60 * 24)) / 365).toString().padStart(2, '0'),
-            days: (Math.floor(diff / (1000 * 60 * 60 * 24)) % 365).toString().padStart(3, '0'),
+            //years: Math.floor(Math.floor(diff / (1000 * 60 * 60 * 24)) / 365).toString().padStart(2, '0'),
+            days: Math.floor(diff / (1000 * 60 * 60 * 24)).toString().padStart(2, '0'),
             hours: Math.floor((diff / (1000 * 60 * 60)) % 24).toString().padStart(2, '0'),
             minutes: Math.floor((diff / (1000 * 60)) % 60).toString().padStart(2, '0'),
             seconds: Math.floor((diff / 1000) % 60).toString().padStart(2, '0'),
         }
         const watch = document.getElementById("watch")
 
-        watch.innerHTML = `<span class="greenColor">${t.years}</span> years : ` +
+        watch.innerHTML = //`<span class="greenColor">${t.years}</span> years : ` +
             `<span class="greenColor">${t.days}</span> days : ` +
             `<span class="greenColor">${t.hours}</span> hours : ` +
             `<span class="greenColor">${t.minutes}</span> minutes : ` +
             `<span class="greenColor">${t.seconds}</span> seconds`
 
-
-        document.title = `${formatDate(birthday)} - ${formatDate(now)}`
+        document.title = `${(queryString.has("date")) ? "Time until " + formatDate(date) : "Time until Jul"}`
     }
     , 100) // Update every 100 milliseconds
 
 function rotationsAroundTheWorld() {
     const daysToOrbitTheSun = 365.25625
 
-    let diff = Date.now() - new Date(year, month, day, hours, minutes)
+    let diff = Date.now() - new Date(year, month, day)
     let years = Math.floor(Math.floor(diff / (1000 * 60 * 60 * 24)) / 365)
     let days = Math.floor(diff / (1000 * 60 * 60 * 24)) % 365
 
@@ -48,5 +51,7 @@ function rotationsAroundTheWorld() {
 }
 
 function formatDate(date) {
-    return `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`
+    const month = date.toLocaleDateString('default', {month: 'long'})
+    return `${date.getDate()}. ${month} ${date.getFullYear()}`
+    //return `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`
 }
